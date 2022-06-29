2465c36e9f4c7560430bab0e4c73542e9b28d45c
Liberty 丶荣耀 <1827768038@qq.com>
2022-06-29 08:38:25 +0000

    航班表

---
+++ ruoyi-ui/src/api/system/flight.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询flight列表
+export function listFlight(query) {
+  return request({
+    url: '/system/flight/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询flight详细
+export function getFlight(flightid) {
+  return request({
+    url: '/system/flight/' + flightid,
+    method: 'get'
+  })
+}
+
+// 新增flight
+export function addFlight(data) {
+  return request({
+    url: '/system/flight',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改flight
+export function updateFlight(data) {
+  return request({
+    url: '/system/flight',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除flight
+export function delFlight(flightid) {
+  return request({
+    url: '/system/flight/' + flightid,
+    method: 'delete'
+  })
+}
+
+// 预订flight
+export function bookFlight(data) {
+  return request({
+    url: '/system/flight',
+    method: 'Patch',
+    data: data
+  })
+}
